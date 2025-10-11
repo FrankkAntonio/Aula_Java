a028efd9dff833ea6de4dabb10b9fc8ad772294a
FrankkAntonio <frankkantonio69@gmail.com>
2025-10-11 11:50:33 -0300

    Organizando

---
+++ Aulas_java/src/notaAula/src/Questao1/Funcionario.java
@@ -0,0 +1,18 @@
+package Questao1;
+
+public class Funcionario {
+    String nome;
+    int matricula;
+    double salarioBruto;
+
+    public void calcularInss(){
+    inss = salarioBruto * 0,15;
+    }
+    public void calcularSalarioLiquido(){
+    salarioliquido = salarioBruto - inss;
+    }
+    public void exibirContraCheque(){
+
+    }
+
+}
---
+++ Aulas_java/src/notaAula/src/Questao1/Main.java
@@ -0,0 +1,22 @@
+package Questao1;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Funcionario f = new Funcionario();
+        double inss;
+        double salarioLiquido;
+
+        System.out.println("Digite o seu nome: ");
+        f.nome = sc.nextLine();
+
+        System.out.println("Digite o sua Matricula: ");
+        f.matricula = sc.nextInt();
+
+        System.out.println("Digite o seu salario: ");
+        f.salarioBruto = sc.nextDouble();
+
+
+
+    }
+}
